9fc570168b4ea28ff9a933b925f5dcaef1eeef0b
ishitasharma27 <124980042+ishitasharma27@users.noreply.github.com>
2023-02-10 11:25:29 +0530

    Add files via upload

---
+++ movie/src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import Counters from "./components/counters";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, vlaue: 0 },
+      { id: 2, vlaue: 0 },
+      { id: 3, vlaue: 0 },
+      { id: 4, vlaue: 0 },
+    ],
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[0] = { ...counter };
+    counters[0].value++;
+    this.setState({ counters });
+  };
+
+  handleDecrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[0] = { ...counter };
+    counters[0].value--;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  render() {
+    console.log("app-rendered");
+    return (
+      <React.Fragment>
+        <NavBar
+          totalCounters={this.state.counters.filter((c) => c.vlaue > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ movie/src/components/counters.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import counter from "./counter";
+
+export class Counters extends Component {
+  render() {
+    const { onReset, counter, onDelete, onIncrement } = this.props;
+
+    return (
+      <div>
+        <button
+          onClick={this.props.onReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
+        {this.props.counters.map((counter) => (
+          <counter
+            key={counter.id}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
+            onDecrement={this.props.onDecrement}
+            counter={counter}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
